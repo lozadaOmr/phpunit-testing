deedd43ce0b0ca97c1ac42e8561d6c5b5640aa3a
lozadaOmr <omrlozada@gmail.com>
2015-04-26 13:12:35 +0800

    Add testTodosDestroy()

--- app/tests/RoutesTest.php
+++ app/tests/RoutesTest.php
@@ -2,6 +2,13 @@
 
 class RoutesTest extends TestCase {
 
+	/**
+	 * The main goal of testing the routes
+	 * is to check endpoints, if it functions as expected
+	 *
+	 * ie. todos/index should be called using get
+	 *     with an expected 200 OK response
+	 */
 	public function testTodosIndex()
 	{
 		$crawler = $this->client->request('GET', '/todos');
@@ -26,6 +33,14 @@ class RoutesTest extends TestCase {
 	public function testTodosEdit()
 	{
 		$crawler = $this->client->request('GET', '/todos/1/edit');
+
+		$this->assertTrue($this->client->getResponse()->isOk());
+	}
+
+	public function testTodosDestroy()
+	{
+		// calling the controller method directly
+		$response = $this->action('DELETE', 'TodosController@destroy', ['id' => 1]);
 
 		$this->assertTrue($this->client->getResponse()->isOk());
 	}
