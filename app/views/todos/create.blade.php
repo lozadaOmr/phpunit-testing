f842327d41cec663e30b3d602a6d807ed4e157a1
lozadaOmr <omrlozada@gmail.com>
2015-05-02 12:25:45 +0800

    Updated views todos/create

--- app/views/todos/create.blade.php
+++ app/views/todos/create.blade.php
@@ -1,1 +1,10 @@
 <h1>Create new Todo</h1>
+
+{{ Form::open(array('route' => 'todos.store')) }}
+	{{ Form::label('title', 'Title') }} <br>
+	{{ Form::text('title', null, array('placeholder' => 'Enter Title Here')) }} <br>
+	<br>
+	{{ Form::label('description', 'Description') }} <br>
+	{{ Form::textarea('description', null) }} <br>
+	{{ Form::submit('Submit') }}
+{{ Form::close() }}
